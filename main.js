06e5d4475532ba8557e0ae0e241a8ece8717c81d
Juan <venegas.a.juan37@gmail.com>
2022-08-11 00:23:41 -0400

    Commit 2-almost there

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const grid = document.querySelector('.grid')
+const reset = document.querySelector('.reset-button')
+let cells = []
 
 generateGame()
 function generateGame(){
-    let cells = []
     let totalMines = (100/10) + 3
     let gridArr = Array(100).fill('value')
     // include mines in grid
@@ -19,53 +20,125 @@ function generateGame(){
         const cell = document.createElement('div')
         cell.setAttribute('id', i)
         cell.classList.add(gridArr[i])
-        cells.push(cell)
         grid.appendChild(cell)
-        cell.addEventListener('click', playGame)
+        cell.addEventListener('click', playGame, {once: true})
+        cells.push(cell)
     }
-    //console.log(cells)    
 
     // add values to each cell
-    let value = 0
     for(i=0; i<100; i++){
+        let value = 0
         if(cells[i].classList.contains('value')){
             // range 9-11 and +-1 on index values except on edge or corner cells
             // if() checking for mines in adjacent squares
-            if()
-            //html data attribute, ref. to w3 schools
+            if(i>10 && i%10!==0 && cells[i-11].classList.contains('mine')) {
+                value+=1
+            }
+            if(i>9 && cells[i-10].classList.contains('mine')) {
+                value+=1
+            }
+            if(i>9 && i%10!==9 && cells[i-9].classList.contains('mine')) {
+                value+=1
+            }
+            if(i>0 && i%10!==0 && cells[i-1].classList.contains('mine')) {
+                value+=1
+            }
+            if(i%10!==9 && cells[i+1].classList.contains('mine')) {
+                value+=1
+            }
+            if(i<90 && i%10!==0 && cells[i+9].classList.contains('mine')) {
+                value+=1
+            }
+            if(i<90 && cells[i+10].classList.contains('mine')) {
+                value+=1
+            }
+            if(i<90 && i%10!==9 && cells[i+11].classList.contains('mine')) {
+                value+=1
+            }
+            //html "data" attribute, ref. to w3 schools
             cells[i].setAttribute('data', value)
         }
     }
-    // console.log(cells)
 }
-    //test code from previous attempt to make this might help
-    // let value = 0
-    // if(j!=0){
-                //     mineGrid[i][j+1]='1' change this to update -> value+=1
-                //     mineGrid[i][j-1]='1'
-                //     mineGrid[i+1][j+1]='1'
-                //     mineGrid[i+1][j-1]='1'
-                // } else if(j==0) {
-                //     mineGrid[i][j+1]='1'
-                //     mineGrid[i+1][j+1]='1'
-                // } 
-                // if(i!=0){
-                //     mineGrid[i+1][j]='1'
-                //     mineGrid[i-1][j]='1'
-                //     mineGrid[i-1][j+1]='1'
-                //     mineGrid[i-1][j-1]='1'
-                // }
-                // if(i!=0 && j!=0){
-
-                // }
+console.log(cells[1])
 
 //either step on mine or earn points
 
 function playGame(e){
-    if(e.target.hasAttribute('mine')){
-        alert('game over')
-        e.target.removeEventListener('click', null)
+    //console.log(e.target.id, document.getElementById(`${parseInt(position)}`))
+    if(e.target.classList.contains('clicked')){
+        return;
     }
-    // alert('game over')
-    // e.target.removeEventListener('click', null)
+    if(e.target.classList.contains('mine')){
+        //e.target.style.backgroundColor = 'red'
+        alert('game over')
+    } 
+    let position = e.target.id
+    //console.log(e.target.getAttribute('data'))
+    //get attribute data pulls out the value assigned to cell according to adjacent mines
+    if(e.target.hasAttribute('data')){
+        e.target.innerHTML = e.target.getAttribute('data')
+        if(e.target.innerHTML != '0'){
+            e.target.classList.add('clicked')
+            return;
+        } 
+        // else {
+        //     if(parseInt(position) > 0 && parseInt(position)%10 !== 0){
+        //         let newPosition = parseInt(position) - 11
+        //         console.log(parseInt(newPosition))
+        //         playGame(document.querySelector(`[id='${newPosition}']`))
+        //     }
+        // }
+
+        // if(e.target.innerHTML == '0'){
+        //     console.log(position)
+            // while(e.target.hasAttribute('data')){
+            //     playGame()
+            //     //e is event, event is click, click is initiated when individual cell is clicked
+            //     //when cell is clicked, if cell is 0, cells around it should also be 'clicked'
+            //     //cell has id number attached to it, access id (set id equal to variable that can 
+            //     // be modified to bring back another id)
+            //     //trigger new click event based on number
+            //     //   .id = `${position + 1}`???
+            // }
+        // }
+
+        if(e.target.innerHTML == '0'){
+            clickedZero(e, position)
+        }
+    }
+    // clickedZero(e, position)
+    e.target.classList.add('clicked')
+}
+
+// console.log(cells[9].id)
+
+// surrounding squares where value is zero or another value
+
+// function clickedZero(e, position){
+//     if(parseInt(position) > 0 && parseInt(position)%10 !== 0){
+//         let newPosition = parseInt(position) - 11
+//         let newCell = document.getElementById(newPosition)
+//         playGame(newCell)
+//     }
+}
+
+// reset button
+reset.addEventListener('click', resetButton)
+function resetButton (e) {
+    location.reload()
+}
+
+// win condition
+
+function winGame(e){
+    for(i=0; i<cells.length; i++){
+        let count = 0
+        if(e.getAttribute('clicked')){
+            count++
+        }
+    }
+    if(count=87){
+    alert('you win')
+    }
 }
